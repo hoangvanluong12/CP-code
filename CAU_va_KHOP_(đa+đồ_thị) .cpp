fc86b5671e5f427c66dd9f82999ed0203677ec7c
hoangvanluong12 <99163388+hoangvanluong12@users.noreply.github.com>
2022-02-14 11:25:12 +0700

    Update CAU_va_KHOP_(đa+đồ_thị) .cpp

--- CAU_va_KHOP_(đa+đồ_thị) .cpp
+++ CAU_va_KHOP_(đa+đồ_thị) .cpp
@@ -1,3 +1,112 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxN = 100008;
+
+vector<int> a[maxN];
+vector<pair<int, int>> res;
+int n, m,
+	number[maxN], low[maxN], parent[maxN], children[maxN],
+	cnt, articulation_point, bridge;
+bool IsCut[maxN];
+
+void DFS(int u, int p)
+{
+    int child = 0;
+	cnt++;
+	number[u] = cnt;
+    low[u] = cnt;
+
+	for(auto v: a[u])
+	{
+		//v = a[u][i];
+
+		if(v != p)
+        {
+            if(number[v])
+                low[u] = min(low[u], number[v]);
+
+            else
+            {
+                DFS(v, u);
+                low[u] = min(low[u], low[v]);
+                child++;
+                if(low[v] >= number[v])
+                {
+                    res.push_back({u, v});
+                    bridge++;
+                }
+                if(u == p)
+                {
+                    if(child >= 2)
+                        IsCut[u] = true;
+                }
+                else if(low[v] >= number[u])
+                    IsCut[u] = true;
+
+            }
+        }
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+    freopen("CUT.inp", "r", stdin);
+    freopen("CUt.out", "w", stdout);
+
+	cin >> n >> m;
+
+	int i, u, v;
+    for(i = 1; i <= m; i++)
+    {
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+
+    for(i =1 ; i <= n; i++)
+        if (number[i] == 0)
+        {
+            DFS(i, i);
+        }
+
+    vector<int> art_points;
+    vector<pair<int, int>> bridges;
+
+    for(auto i: res)
+    {
+        int cnt = 0;
+        int u = i.first, v = i.second;
+        for(auto j: a[u])
+        {
+            if(j == v && ++cnt > 1)
+                break;
+        }
+        if(cnt == 1)
+            bridges.push_back({u, v});
+    }
+
+    for(i = 1; i <= n; i++)
+        if (IsCut[i])
+        {
+            articulation_point++;
+            art_points.push_back(i);
+        }
+
+    cout << bridges.size() << " " << art_points.size() << "\n";
+
+    for(auto res: bridges)
+        cout << res.first << " " << res.second << "\n";
+
+    for(auto v: art_points)
+        cout << v << "\n";
+
+	return 0;
+}
 
 
 // #include <bits/stdc++.h>
